892e643c8f2a099c17c556f4a782ca27706c8b8e
harimandir <harimandir@outlook.com>
2020-10-31 12:17:58 -0700

    Include an example of function return value type inference

--- annotations/functions.ts
+++ annotations/functions.ts
@@ -1,3 +1,9 @@
-const add = (a: number, b: number): number => {
+// return type can be inferred,
+// but this won't help us avoid mistakenly returning the wrong type
+const add = (a: number, b: number) => {
   return a + b;
+};
+
+const subtract = (a: number, b: number): number => {
+  return a - b;
 };
