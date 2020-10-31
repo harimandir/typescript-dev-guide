a5a390a326a8bc9c5aebd8678b86ad05f25f3e9f
harimandir <harimandir@outlook.com>
2020-10-30 22:54:56 -0700

    Include example of variable declaratio before assignment

--- annotations/variables.ts
+++ annotations/variables.ts
@@ -28,3 +28,15 @@ const logNumber: (i: number) => void = (i: number) => console.log(i);
 const json = '{ "x": 10, "y": 20 }';
 const coordinates: { x: number; y: number } = JSON.parse(json);
 console.log(coordinates); // { x: 10, y: 20 }
+
+// 2) When we declare a variable on one line
+// and initialize it later
+let words = ["red", "green", "blue"];
+let foundWord: boolean;
+
+for (let i = 0; i < words.length; i++) {
+  if (words[i] == "green") {
+    foundWord = true;
+    break;
+  }
+}
