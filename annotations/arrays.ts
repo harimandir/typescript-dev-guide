3f6bd64966f08946dddc9c5ae1a6191221a8c281
harimandir <harimandir@outlook.com>
2020-10-31 13:10:05 -0700

    Add examples to demonstrate how annotations help with Array usage

--- annotations/arrays.ts
+++ annotations/arrays.ts
@@ -2,3 +2,14 @@ const carMakers = ["ford", "honda", "general motors"];
 const dates = [new Date(), new Date()];
 
 const carsByMake: string[][] = [];
+
+// type annotation on arrays
+// 1) Helps with inference when extracting values
+const car = carMakers[0];
+const myCar = carMakers.pop();
+
+// 2) Detects incompatible values
+carMakers.push(100);
+
+// 3) Helps with Array method inputs and autocompletion
+carMakers.map((car: string): string => car.toUpperCase());
