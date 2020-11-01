a556d5890fd03c0c4f5982fd7cead8e609108841
harimandir <harimandir@outlook.com>
2020-10-31 23:27:56 -0700

    Display embedded Google Map

--- maps/src/index.ts
+++ maps/src/index.ts
@@ -1,7 +1,7 @@
-import { User } from "./User";
-import { Company } from "./Company";
+// import { User } from "./User";
+// import { Company } from "./Company";
 
-const user = new User();
-const company = new Company();
-
-console.log(user, company);
+new google.maps.Map(document.getElementById("map"), {
+  zoom: 1,
+  center: { lat: 33, lng: -112 },
+});
