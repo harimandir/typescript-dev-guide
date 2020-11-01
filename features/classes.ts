55e920ddf7354f7b49b541830411d80b02b66c90
harimandir <harimandir@outlook.com>
2020-10-31 21:14:19 -0700

    Add example of one line field definition and initialization

--- features/classes.ts
+++ features/classes.ts
@@ -1,12 +1,15 @@
 class Vehicle {
+  constructor(public color: string = "unknown") {}
+
   protected honk(): void {
     console.log("beep");
   }
 }
 
 // Protected methods are only accessible within the class or subclasses
-const vehicle = new Vehicle();
+const vehicle = new Vehicle("white");
 // vehicle.honk();
+console.log(vehicle.color);
 
 class Car extends Vehicle {
   private drive(): void {
@@ -20,3 +23,4 @@ class Car extends Vehicle {
 
 const car = new Car();
 car.startDriving();
+console.log(car.color);
