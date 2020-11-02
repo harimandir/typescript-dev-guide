efba5bf40039852f16d337d60b8c8613bf22c81d
harimandir <harimandir@outlook.com>
2020-11-01 21:13:08 -0700

    Implement a bubble sort

--- sort/src/index.ts
+++ sort/src/index.ts
@@ -2,9 +2,21 @@ class Sorter {
   constructor(protected collection: number[]) {}
 
   sort(): number[] {
+    const { length } = this.collection;
+    for (let i = 0; i < length; i++) {
+      for (let j = 0; j < length - i - 1; j++) {
+        const [x, y] = [this.collection[j], this.collection[j + 1]];
+        if (x > y) {
+          this.collection[j + 1] = x;
+          this.collection[j] = y;
+        }
+      }
+    }
     return this.collection;
   }
 }
 
-const sorter = new Sorter([10, 3, -5, 0]);
+const array = [10, 3, -5, 0];
+console.log(`Sorting ${array}`);
+const sorter = new Sorter(array);
 console.log(sorter.sort());
