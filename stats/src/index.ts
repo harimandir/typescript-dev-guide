21d1fa4e2ee5d9638539ddc25463c2e73bf930e8
harimandir <harimandir@outlook.com>
2020-11-03 16:21:01 -0700

    Output the number of games Chelsea won

--- stats/src/index.ts
+++ stats/src/index.ts
@@ -4,4 +4,15 @@ const matches = fs
   .readFileSync("football.csv", "utf-8")
   .split("\n")
   .map((row: string) => row.split(","));
-console.log(matches);
+
+let chelseaWins = 0;
+
+for (let match of matches) {
+  if (match[1] === "Chelsea" && match[5] === "H") {
+    chelseaWins++;
+  } else if (match[2] === "Chelsea" && match[5] === "A") {
+    chelseaWins++;
+  }
+}
+
+console.log(`Chelsea won ${chelseaWins} games`);
