6bc37648621638e713e0b694fd5cdd93d13ffd23
harimandir <harimandir@outlook.com>
2020-11-03 16:33:40 -0700

    Replace constants with enum

--- stats/src/index.ts
+++ stats/src/index.ts
@@ -5,16 +5,18 @@ const matches = fs
   .split("\n")
   .map((row: string) => row.split(","));
 
-const homeWin = "H";
-const awayWin = "A";
-const draw = "D"; // unused
+enum MatchResult {
+  HomeWin = "H",
+  AwayWin = "A",
+  Draw = "D",
+}
 
 let chelseaWins = 0;
 
 for (let match of matches) {
-  if (match[1] === "Chelsea" && match[5] === homeWin) {
+  if (match[1] === "Chelsea" && match[5] === MatchResult.HomeWin) {
     chelseaWins++;
-  } else if (match[2] === "Chelsea" && match[5] === awayWin) {
+  } else if (match[2] === "Chelsea" && match[5] === MatchResult.AwayWin) {
     chelseaWins++;
   }
 }
