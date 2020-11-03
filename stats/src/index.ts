6f83a87b92127c9293be4cc0f58b2bd1ec366934
harimandir <harimandir@outlook.com>
2020-11-03 16:15:25 -0700

    Load csv data from filesystem

--- stats/src/index.ts
+++ stats/src/index.ts
@@ -1,1 +1,7 @@
-console.log("Hello, World!");
+import fs from "fs";
+
+const matches = fs
+  .readFileSync("football.csv", "utf-8")
+  .split("\n")
+  .map((row: string) => row.split(","));
+console.log(matches);
