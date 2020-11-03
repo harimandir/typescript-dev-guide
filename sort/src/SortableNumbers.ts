12d65ccfd5a9c0e2abe20946e08441409ef3d961
harimandir <harimandir@outlook.com>
2020-11-02 19:36:45 -0700

    Refactor SortableNumbers

---
+++ sort/src/SortableNumbers.ts
@@ -0,0 +1,25 @@
+import { Greater } from "./Sorter";
+
+export class SortableNumbers {
+  constructor(protected numbers: number[]) {}
+
+  get length() {
+    return this.numbers.length;
+  }
+
+  greaterIndexValue(j: number, k: number): Greater {
+    const [x, y] = [this.numbers[j], this.numbers[k]];
+    if (x > y) {
+      return Greater.isLeft;
+    } else if (x < y) {
+      return Greater.isRight;
+    }
+    return Greater.neither;
+  }
+
+  swapIndexValues(j: number, k: number): void {
+    const [x, y] = [this.numbers[j], this.numbers[k]];
+    this.numbers[k] = x;
+    this.numbers[j] = y;
+  }
+}
--- sort/src/index.ts
+++ sort/src/index.ts
@@ -1,29 +1,7 @@
 import { Sorter, Sortable, Greater } from "./Sorter";
+import { SortableNumbers } from "./SortableNumbers";
 
-class SortableNumberArray {
-  length: number;
-  constructor(protected array: number[]) {
-    this.length = array.length;
-  }
-
-  greaterIndexValue(j: number, k: number): Greater {
-    const [x, y] = [this.array[j], this.array[k]];
-    if (x > y) {
-      return Greater.isLeft;
-    } else if (x < y) {
-      return Greater.isRight;
-    }
-    return Greater.neither;
-  }
-
-  swapIndexValues(j: number, k: number): void {
-    const [x, y] = [this.array[j], this.array[k]];
-    this.array[k] = x;
-    this.array[j] = y;
-  }
-}
-
-const array = [10, 3, -5, 0];
-console.log(`Sorting ${array}`);
-const sorter = new Sorter(new SortableNumberArray(array));
+const numbers = [10, 3, -5, 0];
+console.log(`Sorting ${numbers}`);
+const sorter = new Sorter(new SortableNumbers(numbers));
 console.log(sorter.sort());
