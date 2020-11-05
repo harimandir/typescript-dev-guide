3da67582143aa5e7cb929188f4023fc741c17637
harimandir <harimandir@outlook.com>
2020-11-05 11:23:53 -0700

    Implement setter for user props

--- web/models/User.ts
+++ web/models/User.ts
@@ -1,12 +1,16 @@
 interface UserProps {
-  name: string;
-  age: number;
+  name?: string;
+  age?: number;
 }
 
 export class User {
-  constructor(private data: UserProps) {}
+  constructor(private data: UserProps = {}) {}
 
   get(prop: string): string | number {
     return this.data[prop];
+  }
+
+  set(props: UserProps): void {
+    Object.assign(this.data, props);
   }
 }
--- web/src/index.ts
+++ web/src/index.ts
@@ -3,4 +3,5 @@ import { User } from "../models/User";
 console.log("Hello, World!");
 
 const user = new User({ name: "Anon", age: 20 });
+user.set({ age: 21 });
 console.log(user.get("name"), user.get("age"));
