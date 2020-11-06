b110a961d5bcb04145f1da69e9bae636a4a26133
harimandir <harimandir@outlook.com>
2020-11-06 11:50:19 -0700

    Include method to check is user is admin

--- web/src/index.ts
+++ web/src/index.ts
@@ -5,3 +5,5 @@ const user = new User({ id: 1 });
 user.on("change", () => console.log("user changed"));
 
 user.fetch();
+
+console.log(`isAdminUser: ${user.isAdminUser()}`);
--- web/src/models/User.ts
+++ web/src/models/User.ts
@@ -30,4 +30,8 @@ export class User extends Model<UserProps> {
         throw new Error(`Unsupported model type: ${modelType}`);
     }
   }
+
+  isAdminUser(): boolean {
+    return this.get("id") === 1;
+  }
 }
