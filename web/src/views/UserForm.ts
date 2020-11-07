0c384ceb926cca78ee297ba2467087ec5f4a470a
harimandir <harimandir@outlook.com>
2020-11-06 20:27:49 -0700

    Use nested generics implementation for view model

--- web/src/views/UserForm.ts
+++ web/src/views/UserForm.ts
@@ -1,7 +1,7 @@
+import { User, UserProps } from "../models/User";
 import { View } from "../views/View";
-// import { User } from "../models/User";
 
-export class UserForm extends View {
+export class UserForm extends View<User, UserProps> {
   template(): string {
     const name = this.model.get("name");
 
