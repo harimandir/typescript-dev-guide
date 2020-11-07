dc920fa665f06d91aad41805cd033501e79519d6
harimandir <harimandir@outlook.com>
2020-11-06 18:24:09 -0700

    Enable strict null checks with tsconfig

--- web/src/index.ts
+++ web/src/index.ts
@@ -1,8 +1,10 @@
 import { UserForm } from "./views/UserForm";
 import { User } from "./models/User";
 
-const form = new UserForm(
-  document.getElementById("root"),
-  new User({ name: "Morgan", age: 42 })
-);
-form.render();
+const root = document.getElementById("root");
+if (root) {
+  const form = new UserForm(root, new User({ name: "Morgan", age: 42 }));
+  form.render();
+} else {
+  throw new Error("root element not found in document");
+}
