9be8804580e7c8527c25e76dac22ce1fbb1a747a
harimandir <harimandir@outlook.com>
2020-11-08 10:02:28 -0700

    Use the render method of the user detail view to render items

--- web/src/views/Users.ts
+++ web/src/views/Users.ts
@@ -1,30 +1,9 @@
 import { Collection } from "./../views/Collection";
 import { User, UserProps } from "../models/User";
+import { UserDetail } from "./UserDetail";
 
 export class Users extends Collection<User, UserProps> {
-  render(): void {
-    const templateElement = document.createElement("template");
-    const content = templateElement.content;
-    this.parent.textContent = "";
-
-    this.model.models.map((user) => {
-      this.renderItem(templateElement, user);
-    });
-
-    this.parent.append(content);
-  }
-
   renderItem(parent: Element, user: User): void {
-    const templateElement = document.createElement("template");
-    const content = templateElement.content;
-
-    templateElement.innerHTML = `
-    <div class="row">
-      <div class="col-1">Name: ${user.get("name")}</div>
-      <div class="col-2">Age: ${user.get("age")}</div>
-    </div>
-`;
-
-    this.parent.append(content);
+    new UserDetail(parent, user).render();
   }
 }
