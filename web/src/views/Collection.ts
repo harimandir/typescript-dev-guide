72083367d9f498fc02ce6832db0a27607848f98a
harimandir <harimandir@outlook.com>
2020-11-08 10:01:10 -0700

    Add the render method to the abstract collection

--- web/src/views/Collection.ts
+++ web/src/views/Collection.ts
@@ -1,7 +1,21 @@
 import { Collection as Model } from "./../models/Collection";
 
 export abstract class Collection<T, K> {
-  render(): void {}
   abstract renderItem(parent: Element, model: T): void;
+
   constructor(public parent: Element, protected model: Model<T, K>) {}
+
+  render(): void {
+    const templateElement = document.createElement("template");
+    const content = templateElement.content;
+    this.parent.textContent = "";
+
+    this.model.models.map((user) => {
+      const itemElement = document.createElement("div");
+      this.renderItem(itemElement, user);
+      content.append(itemElement);
+    });
+
+    this.parent.append(content);
+  }
 }
