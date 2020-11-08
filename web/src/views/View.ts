85ed9caf5cc8aa6f3a64fe3f2feb922bbe074fb0
harimandir <harimandir@outlook.com>
2020-11-07 19:11:33 -0700

    Map content elements on render

--- web/src/views/View.ts
+++ web/src/views/View.ts
@@ -1,6 +1,8 @@
 import { Model } from "../models/Model";
 
 export abstract class View<T extends Model<K>, K> {
+  contentElements: { [contentName: string]: Element } = {};
+
   abstract template(): string;
 
   constructor(public parent: Element, public model: T) {
@@ -15,7 +17,7 @@ export abstract class View<T extends Model<K>, K> {
     this.model.on("change", () => this.render());
   }
 
-  bindEvents(fragment: DocumentFragment): void {
+  protected bindEvents(fragment: DocumentFragment): void {
     const events = this.events();
     for (let key in events) {
       const [eventName, selector] = key.split(":");
@@ -30,12 +32,29 @@ export abstract class View<T extends Model<K>, K> {
     const templateElement = document.createElement("template");
     templateElement.innerHTML = this.template();
     const content = templateElement.content;
+
     this.bindEvents(content);
+    this.bindContentElements(content);
 
     if (this.parent.firstElementChild) {
       this.parent.replaceChild(content, this.parent.firstElementChild);
     } else {
       this.parent.append(content);
     }
+  }
+
+  bindContentElements(content: DocumentFragment): void {
+    const selectors = this.contentSelectors();
+
+    for (let key in selectors) {
+      const element = content.querySelector(selectors[key]);
+      if (element) {
+        this.contentElements[key] = element;
+      }
+    }
+  }
+
+  contentSelectors(): { [contentName: string]: string } {
+    return {};
   }
 }
