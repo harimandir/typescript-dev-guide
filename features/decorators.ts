645ef4a2defd92ecb30299f4fb3536b9f238921e
harimandir <harimandir@outlook.com>
2020-11-12 15:00:10 -0700

    Convert logError decorator to a decorator factory implementation

--- features/decorators.ts
+++ features/decorators.ts
@@ -6,24 +6,24 @@ module decorators {
       return `This boat is ${this.color}`;
     }
 
-    @logError
+    @logError("sunk")
     pilot(): void {
-      throw new Error("sunk");
+      throw new Error();
       console.log("float");
     }
   }
 
-  function logError(target: any, key: string, desc: PropertyDescriptor): void {
-    const method = desc.value;
+  function logError(message: string) {
+    return function (target: any, key: string, desc: PropertyDescriptor): void {
+      const method = desc.value;
 
-    desc.value = function () {
-      try {
-        method();
-      } catch (e: any) {
-        if (e instanceof Error) {
-          console.log(e.message);
+      desc.value = function () {
+        try {
+          method();
+        } catch (e: any) {
+          console.log(message);
         }
-      }
+      };
     };
   }
 
