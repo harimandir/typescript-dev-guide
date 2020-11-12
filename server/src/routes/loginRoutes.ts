b936d837856efa819d0fe53936fc349cdc82ebf5
harimandir <harimandir@outlook.com>
2020-11-11 23:10:33 -0700

    Add a login form that returns input values using body parser

--- server/src/routes/loginRoutes.ts
+++ server/src/routes/loginRoutes.ts
@@ -2,8 +2,25 @@ import { Router, Request, Response } from "express";
 
 const router = Router();
 
-router.get("/", (req: Request, res: Response) => {
-  res.send(`login routes`);
+router.get("/login", (req: Request, res: Response) => {
+  res.send(`
+    <form method="post">
+      <div>
+        <label>Email</label>
+        <input name="email" />
+      </div>
+      <div>
+        <label>Password</label>
+        <input name="password" type="password" />
+      </div>
+      <button>Submit</button>
+    </form>
+  `);
+});
+
+router.post("/login", (req: Request, res: Response) => {
+  const { email, password } = req.body;
+  res.send({ email, password });
 });
 
 export { router };
