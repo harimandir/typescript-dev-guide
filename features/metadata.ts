4753da02243e41fc28ba63b2149ef62652e33bed
harimandir <harimandir@outlook.com>
2020-11-12 18:47:04 -0700

    Include an example of pulling method metadata with a class decorator

--- features/metadata.ts
+++ features/metadata.ts
@@ -19,4 +19,40 @@ module metadata {
   );
 
   console.log(Reflect.getMetadata("propertyMetadata", something, "value"));
+
+  @printMetadata
+  class House {
+    color = "white";
+
+    @secretPassword("open sesame")
+    visit(): void {
+      console.log("knock, knock");
+    }
+  }
+
+  function secretPassword(password: string) {
+    return function (target: House, key: string) {
+      Reflect.defineMetadata("secretPassword", password, target, "visit");
+    };
+  }
+
+  console.log(
+    "House.prototype.visit secretPassword == ",
+    Reflect.getMetadata("secretPassword", House.prototype, "visit")
+  );
+
+  function printMetadata(constructor: typeof House) {
+    for (let key in constructor.prototype) {
+      const secret = Reflect.getMetadata(
+        "secretPassword",
+        constructor.prototype,
+        key
+      );
+      if (secret !== undefined) {
+        console.log(
+          `printMetadata secretPassword found for House.prototype.${key} == ${secret}`
+        );
+      }
+    }
+  }
 }
