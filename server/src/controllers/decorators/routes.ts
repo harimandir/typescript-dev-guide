ec0e80ff160e6c2c4916ed762dd8c385f30aecb0
harimandir <harimandir@outlook.com>
2020-11-12 19:57:16 -0700

    Add decorators for different http request methods

--- server/src/controllers/decorators/routes.ts
+++ server/src/controllers/decorators/routes.ts
@@ -1,7 +1,16 @@
 import "reflect-metadata";
 
-export function get(path: string): Function {
-  return function (target: any, key: string, desc: PropertyDescriptor): void {
-    Reflect.defineMetadata("path", path, target, key);
+function bindRouteMethod(method: string) {
+  return function (path: string): Function {
+    return function (target: any, key: string, desc: PropertyDescriptor): void {
+      Reflect.defineMetadata("method", method, target, key);
+      Reflect.defineMetadata("path", path, target, key);
+    };
   };
 }
+
+export const get = bindRouteMethod("get");
+export const post = bindRouteMethod("post");
+export const put = bindRouteMethod("put");
+export const patch = bindRouteMethod("patch");
+export const del = bindRouteMethod("delete");
