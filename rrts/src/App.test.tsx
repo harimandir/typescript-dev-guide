c68be00dd6a1b4edec23374bbdf8ada50eebda4f
harimandir <harimandir@outlook.com>
2020-11-14 16:50:05 -0700

    Re-apply index coursework in App
    This allows things to pick up with the course where they left off,
    and proceed with hot reloading working as expected.

--- rrts/src/App.test.tsx
+++ rrts/src/App.test.tsx
@@ -1,9 +1,9 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
+test("renders the app container in the document", () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const appContainer = screen.getByTestId("App");
+  expect(appContainer).toBeInTheDocument();
 });
