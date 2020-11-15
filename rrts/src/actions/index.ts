f90b6898ab4aab6d49c75925c9cb8e9b36ef302d
harimandir <harimandir@outlook.com>
2020-11-14 21:12:38 -0700

    Add action types enum and type annotation for fetching todos

--- rrts/src/actions/index.ts
+++ rrts/src/actions/index.ts
@@ -1,11 +1,18 @@
 import axios from "axios";
 import { Dispatch } from "redux";
+import { ActionTypes } from "./ActionTypes";
+
+interface Todo {
+  id: number;
+  title: string;
+  completed: boolean;
+}
 
 export const fetchTodos = () => {
   return async (dispatch: Dispatch) => {
-    const response = await axios.get(
+    const response = await axios.get<Todo[]>(
       "http://jsonplaceholder.typicode.com/todos"
     );
-    dispatch({ type: "FETCH_TODOS", payload: response.data });
+    dispatch({ type: ActionTypes.fetchTodos, payload: response.data });
   };
 };
