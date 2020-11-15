c0648bddd843f6ead06cf8d3f133bbef32af6168
harimandir <harimandir@outlook.com>
2020-11-14 23:44:05 -0700

    Handle todo deletion in todos reducer

--- rrts/src/reducers/todos.ts
+++ rrts/src/reducers/todos.ts
@@ -1,11 +1,10 @@
 import { Todo } from "../types/Todo";
-import { ActionTypes, FetchTodosAction } from "../actions";
+import { ActionTypes, Action } from "../actions";
 
-export const todosReducer = (
-  state: Todo[] = [],
-  action: FetchTodosAction
-): Todo[] => {
+export const todosReducer = (state: Todo[] = [], action: Action): Todo[] => {
   switch (action.type) {
+    case ActionTypes.deleteTodo:
+      return state.filter((todo) => todo.id !== action.payload);
     case ActionTypes.fetchTodos:
       return action.payload;
     default:
