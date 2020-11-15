7f2d084ffa9017e7f863fe43213c1eeb8a9474de
harimandir <harimandir@outlook.com>
2020-11-14 17:15:07 -0700

    Implement a Redux store with a pared down app component

--- rrts/src/App.tsx
+++ rrts/src/App.tsx
@@ -1,31 +1,8 @@
 import React from "react";
 
-interface AppProps {
-  prop?: string;
-}
-
-class App extends React.Component<AppProps> {
-  state = { value: 0 };
-
-  handleIncrement = (): void => {
-    this.setState({ value: this.state.value + 1 });
-  };
-
-  handleDecrement = (): void => {
-    this.setState({ value: this.state.value - 1 });
-  };
-
+class App extends React.Component {
   render() {
-    return (
-      <div data-testid="App">
-        props.prop == {this.props.prop}?
-        <div>
-          <button onClick={this.handleIncrement}>Increment value</button>
-          <button onClick={this.handleDecrement}>Decrement value</button>
-        </div>
-        state.value == {this.state.value}
-      </div>
-    );
+    return <div data-testid="App">Hello, World!</div>;
   }
 }
 
